3f08c517bd7d9df4ea684fcaab398186c87faa1a
AlfredoPazZamora <apaz0@ucol.mx>
2020-02-11 23:14:07 -0600

    otro comentario

--- tienda-ecommerce/app/Http/Kernel.php
+++ tienda-ecommerce/app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 
+    //otro comentario 
     /**
      * The priority-sorted list of middleware.
      *
